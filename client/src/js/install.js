66201afea9082cd838c4e198284446a858ef929d
JBlay10 <joseblay10@gmail.com>
2023-01-09 14:12:08 -0600

    Fixed icons

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,7 +1,7 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
+// Event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     event.preventDefault();
     console.log('👍', 'beforeinstallprompt', event);
@@ -10,7 +10,7 @@ window.addEventListener('beforeinstallprompt', (event) => {
     butInstall.classList.toggle('hidden', false);
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
+// Click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
     const eventPrompt = window.defferredPrompt;
 
@@ -23,7 +23,7 @@ butInstall.addEventListener('click', async () => {
     butInstall.classList.toggle('hidden', true);
 });
 
-// TODO: Add an handler for the `appinstalled` event
+// Handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
     window.deferredPrompt = null;
 
