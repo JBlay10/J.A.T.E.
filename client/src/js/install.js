a2fc2964cbfc7986f2c363c7b2f5785b14d238dd
JBlay10 <joseblay10@gmail.com>
2023-01-11 23:21:16 -0600

    fixing install button

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -5,23 +5,25 @@ const butInstall = document.getElementById('buttonInstall');
 window.addEventListener('beforeinstallprompt', (event) => {
     event.preventDefault();
     // console.log('👍', 'beforeinstallprompt', event);
-
-    window.deferredPrompt = event;
-    butInstall.classList.toggle('hidden', false);
+    // window.deferredPrompt = event;
+    // butInstall.classList.toggle('hidden', false);
+    butInstall.style.visibility = 'visible';
+    butInstall.textContent = 'Install'
 });
 
 // Click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-    const eventPrompt = window.deferredPrompt;
-    eventPrompt.prompt();
+    // const eventPrompt = window.deferredPrompt;
+    // eventPrompt.prompt();
 
-    window.deferredPrompt = null;
-    butInstall.classList.toggle('hidden', true);
+    // window.deferredPrompt = null;
+    // butInstall.classList.toggle('hidden', true);
+    butInstall.setAttribute('disabled', true);
+    butInstall.textContent = "Installed";
 });
 
 // Handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
-    window.deferredPrompt = null;
 
     console.log('Awesome', 'appinstalled', event);
 });
